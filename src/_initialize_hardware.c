b3d9e7ed8b492b670143ab5cd6a1e367a8e26799
Leigh McKenzie <maccarav0@gmail.com>
2017-07-28 23:17:45 +1000

    Initial blinky project
    Initial project setup using STM32F7 blinky template from provided by
    the MCU GNU eclipse plugin.

---
+++ src/_initialize_hardware.c
@@ -0,0 +1,202 @@
+/*
+ * This file is part of the µOS++ distribution.
+ *   (https://github.com/micro-os-plus)
+ * Copyright (c) 2014 Liviu Ionescu.
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or
+ * sell copies of the Software, and to permit persons to whom
+ * the Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+// ----------------------------------------------------------------------------
+
+#include "stm32f7xx.h"
+#include "stm32f7xx_hal.h"
+#include "stm32f7xx_hal_cortex.h"
+
+// ----------------------------------------------------------------------------
+
+// The external clock frequency is specified as a preprocessor definition
+// passed to the compiler via a command line option (see the 'C/C++ General' ->
+// 'Paths and Symbols' -> the 'Symbols' tab, if you want to change it).
+// The value selected during project creation was HSE_VALUE=216000000.
+//
+// The code to set the clock is at the end.
+//
+// Note1: The default clock settings assume that the HSE_VALUE is a multiple
+// of 1MHz, and try to reach the maximum speed available for the
+// board. It does NOT guarantee that the required USB clock of 48MHz is
+// available. If you need this, please update the settings of PLL_M, PLL_N,
+// PLL_P, PLL_Q to match your needs.
+//
+// Note2: The external memory controllers are not enabled. If needed, you
+// have to define DATA_IN_ExtSRAM or DATA_IN_ExtSDRAM and to configure
+// the memory banks in system/src/cmsis/system_stm32f7xx.c to match your needs.
+
+// ----------------------------------------------------------------------------
+
+// Forward declarations.
+
+void __initialize_hardware (void);
+
+void __initialize_hardware_early (void);
+
+void SystemClock_Config (void);
+
+static void Error_Handler(void);
+// ----------------------------------------------------------------------------
+
+#if 0
+
+// These are the very first initialisations, performed immediately after
+// reset.
+//
+// Redefine this function to enable external RAM before data & bss init.
+
+void
+__initialize_hardware_early (void)
+{
+}
+
+#endif
+
+// This is the application hardware initialisation routine,
+// redefined to add more inits.
+//
+// Called from _start(), right after data & bss init, before
+// constructors.
+//
+// After reset the Cortex-M processor is in Thread mode,
+// priority is Privileged, and the Stack is set to Main.
+//
+// Warning: The HAL requires the system timer, running at 1000 Hz
+// and calling HAL_IncTick().
+
+void
+__initialize_hardware (void)
+{
+  // Enable instruction & data cache.
+  SCB_EnableICache ();
+  SCB_EnableDCache ();
+
+  // Initialise the HAL Library; it must be the first function
+  // to be executed before the call of any HAL function.
+  // More HAL initialisations can be defined in stm32f7xx_hal_msp.c
+  HAL_Init ();
+
+  // Enable HSE Oscillator and activate PLL with HSE as source
+  SystemClock_Config ();
+
+  // Call the CSMSIS system clock routine to store the clock frequency
+  // in the SystemCoreClock global RAM location.
+  SystemCoreClockUpdate ();
+}
+
+// Disable when using RTOSes, since they have their own handler.
+#if 0
+
+// This is a sample SysTick handler, use it if you need HAL timings.
+void __attribute__ ((section(".after_vectors")))
+SysTick_Handler(void)
+  {
+#if defined(USE_HAL_DRIVER)
+    HAL_IncTick();
+#endif
+  }
+
+#endif
+
+// ----------------------------------------------------------------------------
+
+/**
+  * @brief  System Clock Configuration
+  *         The system Clock is configured as follow :
+  *            System Clock source            = PLL (HSE)
+  *            SYSCLK(Hz)                     = 216000000
+  *            HCLK(Hz)                       = 216000000
+  *            AHB Prescaler                  = 1
+  *            APB1 Prescaler                 = 4
+  *            APB2 Prescaler                 = 2
+  *            HSE Frequency(Hz)              = 8000000
+  *            PLL_M                          = 8
+  *            PLL_N                          = 432
+  *            PLL_P                          = 2
+  *            PLL_Q                          = 9
+  *            PLL_R                          = 7
+  *            VDD(V)                         = 3.3
+  *            Main regulator output voltage  = Scale1 mode
+  *            Flash Latency(WS)              = 7
+  * @param  None
+  * @retval None
+  */
+void SystemClock_Config(void)
+{
+  RCC_ClkInitTypeDef RCC_ClkInitStruct;
+  RCC_OscInitTypeDef RCC_OscInitStruct;
+
+  /* Enable HSE Oscillator and activate PLL with HSE as source */
+  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSE;
+  RCC_OscInitStruct.HSEState = RCC_HSE_BYPASS;
+  RCC_OscInitStruct.HSIState = RCC_HSI_OFF;
+  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_ON;
+  RCC_OscInitStruct.PLL.PLLSource = RCC_PLLSOURCE_HSE;
+  RCC_OscInitStruct.PLL.PLLM = 8;
+  RCC_OscInitStruct.PLL.PLLN = 432;
+  RCC_OscInitStruct.PLL.PLLP = RCC_PLLP_DIV2;
+  RCC_OscInitStruct.PLL.PLLQ = 9;
+  RCC_OscInitStruct.PLL.PLLR = 7;
+  if(HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+  /* Activate the OverDrive to reach the 216 Mhz Frequency */
+  if(HAL_PWREx_EnableOverDrive() != HAL_OK)
+  {
+    Error_Handler();
+  }
+
+
+  /* Select PLL as system clock source and configure the HCLK, PCLK1 and PCLK2
+     clocks dividers */
+  RCC_ClkInitStruct.ClockType = (RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2);
+  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
+  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
+  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV4;
+  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV2;
+  if(HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_7) != HAL_OK)
+  {
+    Error_Handler();
+  }
+}
+
+/**
+  * @brief  This function is executed in case of error occurrence.
+  * @param  None
+  * @retval None
+  */
+static void Error_Handler(void)
+{
+  while(1)
+  {
+  }
+}
+
+// ----------------------------------------------------------------------------
